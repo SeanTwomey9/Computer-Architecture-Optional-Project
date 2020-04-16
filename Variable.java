7e408936da44ed8d107090e7b1911a855f1dd9a0
nwibl <63751151+nwibl@users.noreply.github.com>
2020-04-16 17:24:06 -0400

    Update Variable.java

--- Variable.java
+++ Variable.java
@@ -1,1 +1,12 @@
-
+public class Variable
+{
+  String name;
+  int value;
+  char datatype;
+  Variable(String name, int value, char datatype)
+  {
+    this.name = name;
+    this.value = value;
+    this.datatype = datatype;
+  }
+}
