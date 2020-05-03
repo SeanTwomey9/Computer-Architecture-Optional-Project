b28ee3c23c5bd6002ae632a8c3216cec5d5a401e
nwibl <63751151+nwibl@users.noreply.github.com>
2020-05-03 16:35:07 -0400

    Update MIPSConversion.java

--- MIPSConversion.java
+++ MIPSConversion.java
@@ -1,10 +1,9 @@
 import java.util.ArrayList;
 
 public class MIPSConversion {
-	private ArrayList<String> tokens;
-	private ArrayList<String> writtenMIPs;
-	private ArrayList<String> holdAlways;
-	private int alwaysCounter;
+	ArrayList<String> tokens;
+	ArrayList<String> writtenMIPs;
+	VariableList varList = new VariableList();
 	MIPSConversion(ArrayList<String> arr)
 	{
 		tokens = arr;
@@ -14,47 +13,36 @@ public class MIPSConversion {
 		int length = tokens.size();
 		for(int i = 0; i < length; i ++)
 		{
-			testLine(tokens.get(i));
+			//testForAlways(tokens.get(i));
+			testForVariable(tokens.get(i));
+			
 		}
 		
 	}
-	void testLine(String line)
+	void testForVariable(String line)
 	{
 		String command = "";
 		int k = 0;
-		while(line.charAt(k + 1) != ' ')
+		while(line.charAt(k) != ' ')
 		{
 			command += line.charAt(k);
 			k ++;
 		}
-		if(command=="always")
-			//go somewhere
-	}
-	public void findAlways() {
-
-		String b = "begin";
-		String e = "end";
-		int alwaysIndex;
-		
-		for(int i = 0; i < tokens.size(); i++) {
-			
-			if(tokens.contains(b)) {
-				
-				alwaysIndex = i;
-				
-			}
-			
-			while(tokens.equals(b) || !tokens.equals(e)) {
-				
-				holdAlways.set(i, tokens.get(i));
-			}
+		line = line.substring(k+1,line.length());
+		switch(command.toLowerCase())
+		{
+		case "input":
+			varList.addInputVar(line);
+		case "output":
+			varList.addOutputVar(line);
+		case "reg":
+			varList.addRegisterVar(line);
+		case "wire":
+			varList.addWireVar(line);
+		case "parameter":
+			varList.addParameter(line);
+		default:
+			return;
 		}
 	}
-	
-	public void concatenateAlways() {
-		
-		if(alwaysCounter > 1) {
-			
-		}
-}
 }
