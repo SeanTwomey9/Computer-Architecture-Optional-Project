4cd96797d8bac6a74bd47c18123904872a82422c
nwibl <63751151+nwibl@users.noreply.github.com>
2020-05-06 14:49:32 -0400

    Update MIPSConversion.java

--- MIPSConversion.java
+++ MIPSConversion.java
@@ -23,7 +23,15 @@ public class MIPSConversion {
 			
 			return lines;
 		}
-	void testLine(String line)
+	void parseLines()
+	{
+		int length = lines.size();
+		for(int i = 0; i < length; i ++)
+		{
+			testLine(lines.get(i), i);
+		}
+	}
+	void testLine(String line, int loc)
 	{
 		String command = "";
 		int k = 0;
@@ -32,20 +40,14 @@ public class MIPSConversion {
 			command += line.charAt(k);
 			k ++;
 		}
-		command=command.toLowercase();
+		line = line.substring(k+1);
+		command=command.toLowerCase();
 		if(command=="always")
-			testForAlways(line, command);
-		testForVariable(line, command)
-	}
-	void testForAlways(String line)
-	{
-		Always temp = new Always();
-		
+			//testForAlways(line, command);
+		testForVariable(line, command, loc);
 	}
 	void testForVariable(String line, String command)
 	{
-		
-		line = line.substring(k+1,line.length());
 		switch(command.toLowerCase())
 		{
 		case "input":
