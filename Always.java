f6e8fbf1919332327abcc198cfb60bfc9f5e0e48
Sean Twomey <56229866+SeanTwomey9@users.noreply.github.com>
2020-05-06 14:45:41 -0400

    Update Always.java

--- Always.java
+++ Always.java
@@ -4,8 +4,9 @@ import java.io.*;
 
 public class Always {
 
-	private static ArrayList<String> lines;
-	private static int [] bookEnds = new int[2];
+	private static ArrayList<String> fileLines = MipsConversion.getLines();
+	private static ArrayList<String> input;
+	private static ArrayList<String> output;
 	private static String condition = "";
 	
 	public Always(String condition) {
@@ -13,25 +14,31 @@ public class Always {
 		this.condition = condition;
 	}
 	
-	
 	public static void findCondition() {
 		
 		String beginCondition = "(";
 		String endCondition = ")";
 		
 		
-		for(int i = 0; i < lines.size(); i ++) {
+		for(int i = 0; i < fileLines.size(); i ++) {
 			
-			while(lines.get(i).equals(beginCondition) && (!lines.get(i).equals(endCondition))) {
+			while(fileLines.get(i).equals(beginCondition) && (!fileLines.get(i).equals(endCondition))) {
 				
-				condition = lines.get(i);
+				condition = fileLines.get(i);
 				condition = condition.toLowerCase();
 			}
 		}
 	}
-	public void compareConditions(String con)
-	{
+	
+	public static void compareConditions(String con) {
+		
 		if(condition.contentEquals(con))
 			//test for time delays and determine order
+			
+	}
+	
+	public static void findInputs() {
+		
+		
 	}
 }
