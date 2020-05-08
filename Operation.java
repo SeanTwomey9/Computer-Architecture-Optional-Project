ee7a0cd610adef623fe2d982f219fa1f2cdc8b8a
Sean Twomey <56229866+SeanTwomey9@users.noreply.github.com>
2020-05-08 17:38:42 -0400

    Update Operation.java
    Commented

--- Operation.java
+++ Operation.java
@@ -1,9 +1,23 @@
 import java.util.ArrayList;
 
+/**
+ * Defines the operations occuring in MIPS which verilog statements are assigned to
+ * @author neilwible + seantwomey
+ *
+ */
 public class Operation {
+	//output variable
 	String output;
+	//Array list of strings containing input variables
 	ArrayList<String> input;
+	//location of operation in file
 	int location;
+	/**
+	 * Constructor for an operation: takes the command(output), the line which identifies inputs, and location of the operation
+	 * @param command
+	 * @param line
+	 * @param loc
+	 */
 	Operation(String command, String line, int loc)
 	{
 		output = command;
@@ -11,46 +25,33 @@ public class Operation {
 		input = new ArrayList<String>();
 		findInputs(line);
 	}
+	
+	/**
+	 * Identifies inputs of a MIPS operation based on inputted line
+	 * @param line
+	 */
 	void findInputs(String line)
 	{
+		if(line.charAt(0) == '=') {
+			
 		line = line.substring(2);
+		}
 		int i = 0;
 		int lastIndex = 0;
 		input.add("");
-		while(line.charAt(i) != ';')
-		{
-			if(line.charAt(i) == '(')
-			{
-				line = line.substring(i);
-				input.add("begin");
-				line = testParenthesis(line.substring(1));
-				input.add("end");
-				input.add("");
-				lastIndex = input.size() - 1;
-				i = 0;
-			}
-			else
+		
+			while(line.charAt(i) != ';') //iterates until the semi-colon at end of line is reached
 			{
 				input.set(lastIndex, input.get(lastIndex).concat(Character.toString(line.charAt(i))));
+				//if no parentheses, append last character of the input 
 				i ++;
 			}
+			
 		}
-	}
-	String testParenthesis(String line)
-	{
-		int i = 0;
-		while(line.charAt(i) != ')')
-		{
-			if(line.charAt(i) == '(')
-			{
-				line = line.substring(0,i) + testParenthesis(line.substring(i+1));
-				i --;
-			}
-			i ++;
-		}
-		input.add(line.substring(0, i));
-		return line.substring(i + 1);
-	}
+	
+	/**
+	 * Simply returns the inputs
+	 */
 	void outputInput()
 	{
 		for(int i = 0; i < input.size(); i ++)
