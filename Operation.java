57ad0b0f12dc214c7388d3bc8c5b08d717d943c8
nwibl <63751151+nwibl@users.noreply.github.com>
2020-05-08 17:46:42 -0400

    Update Operation.java

--- Operation.java
+++ Operation.java
@@ -6,18 +6,9 @@ import java.util.ArrayList;
  *
  */
 public class Operation {
-	//output variable
 	String output;
-	//Array list of strings containing input variables
 	ArrayList<String> input;
-	//location of operation in file
 	int location;
-	/**
-	 * Constructor for an operation: takes the command(output), the line which identifies inputs, and location of the operation
-	 * @param command
-	 * @param line
-	 * @param loc
-	 */
 	Operation(String command, String line, int loc)
 	{
 		output = command;
@@ -25,33 +16,19 @@ public class Operation {
 		input = new ArrayList<String>();
 		findInputs(line);
 	}
-	
-	/**
-	 * Identifies inputs of a MIPS operation based on inputted line
-	 * @param line
-	 */
 	void findInputs(String line)
 	{
-		if(line.charAt(0) == '=') {
-			
-		line = line.substring(2);
-		}
+		if(line.charAt(0) == '=')
+			line = line.substring(2);
 		int i = 0;
 		int lastIndex = 0;
 		input.add("");
-		
-			while(line.charAt(i) != ';') //iterates until the semi-colon at end of line is reached
-			{
-				input.set(lastIndex, input.get(lastIndex).concat(Character.toString(line.charAt(i))));
-				//if no parentheses, append last character of the input 
-				i ++;
-			}
-			
+		while(line.charAt(i) != ';')
+		{
+			input.set(lastIndex, input.get(lastIndex).concat(Character.toString(line.charAt(i))));
+			i ++;
 		}
-	
-	/**
-	 * Simply returns the inputs
-	 */
+	}
 	void outputInput()
 	{
 		for(int i = 0; i < input.size(); i ++)
