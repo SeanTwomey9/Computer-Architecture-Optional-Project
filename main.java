29e48f073404518e303e711177b6565810d43807
nwibl <63751151+nwibl@users.noreply.github.com>
2020-05-08 17:47:26 -0400

    Update main.java

--- main.java
+++ main.java
@@ -3,8 +3,7 @@ public class main {
 
 	public static void main(String[] args) throws Exception {
 		Lexical_Analysis lex = new Lexical_Analysis("C:\\Users\\Neil\\Desktop\\test.txt");	//Set string to location of file to be tested
-		ArrayList<String> lines = lex.runLexicalAnalysis();
-		
+		MIPSConversion con = new MIPSConversion(lex.runLexicalAnalysis());
+		con.parseLines();
 	}
-
 }
