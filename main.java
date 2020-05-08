82ef764c6114526c1ade323a4fce1379a4b3547a
Sean Twomey <56229866+SeanTwomey9@users.noreply.github.com>
2020-05-08 18:05:01 -0400

    Update main.java
    Commented

--- main.java
+++ main.java
@@ -1,8 +1,14 @@
-
+/**
+ * Main method for executing compiler to convert Verilog code to MIPS
+ * @author neilwible + seantwomey
+ *
+ */
 public class main {
 
 	public static void main(String[] args) throws Exception {
-		Lexical_Analysis lex = new Lexical_Analysis("C:\\Users\\Neil\\Desktop\\test.txt");	//Set string to location of file to be tested
+		//runs initial lexical analysis on input text file 
+		Lexical_Analysis lex = new Lexical_Analysis("/Users/seantwomey/Desktop/CWRU /Junior Year/Spring 2020/EECS 314/Project/test.txt");	//Set string to location of file to be tested
+		//executes MIPSConversion to convert verilog file to functional MIPS
 		MIPSConversion con = new MIPSConversion(lex.runLexicalAnalysis());
 		con.parseLines();
 	}
